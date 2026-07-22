6d67a3cc69424071dec087ac05873c47700f638e
Tatiana <vpulgua@gmail.com>
2026-07-22 11:33:12 +0200

    Push 2 negative tests

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,5 +1,6 @@
 package ui_tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
@@ -16,10 +17,109 @@ public class LoginTests extends TestBase {
                 .fillPassword("AitWebsiteCohort82!!")
                 .clickLoginButton();
 
-
-        app.getHomePage()
-                .clickUserMenu()
-                .clickMyAccount();
-
+        Assert.assertTrue(
+                app.getHomePage().isUserMenuDisplayed(),
+                "Expected authorized user menu to be displayed after login.");
     }
+
+    //Первый негативный тест
+    @Test
+    public void loginWithEmptyEmailAndPasswordNegativeTest() {
+        app.getHomePage()
+                .openHomePage()
+                .acceptCookies()
+                .clickLoginButton();
+
+        app.getLoginPage()
+                .clickLoginWithEmail()
+                .clickLoginButton();
+
+        Assert.assertEquals(
+                app.getLoginPage().getEmailValidationMessage(),
+                "Email cannot be blank"
+        );
+
+        Assert.assertEquals(
+                app.getLoginPage().getPasswordValidationMessage(),
+                "Make sure you enter a password."
+        );
+    }
+
+    @Test
+    public void loginWithInvalidPasswordNegativeTest() {
+        app.getHomePage()
+                .openHomePage()
+                .acceptCookies()
+                .clickLoginButton();
+
+        app.getLoginPage()
+                .clickLoginWithEmail()
+                .fillEmail("aitwebsitecohort82m@gmail.com")
+                .fillPassword("WrongPassword123!")
+                .clickLoginButton();
+
+        Assert.assertEquals(
+                app.getLoginPage().getWrongCredentialsMessage(),
+                "Wrong email or password",
+                "Incorrect error message for wrong email or password.");
+    }
+
+    @Test
+    public void loginWithNonExistingEmailNegativeTest() {
+        app.getHomePage()
+                .openHomePage()
+                .acceptCookies()
+                .clickLoginButton();
+
+        app.getLoginPage()
+                .clickLoginWithEmail()
+                .fillEmail("notregistered" + System.currentTimeMillis() + "@gmail.com")
+                .fillPassword("AitWebsiteCohort82!!")
+                .clickLoginButton();
+
+        Assert.assertEquals(
+                app.getLoginPage().getNonExistingEmailMessage(),
+                "This email doesn't match any account. Try again.",
+                "Incorrect error message for a non-existing email.");
+    }
+
+    @Test
+    public void loginWithEmptyEmailNegativeTest() {
+        app.getHomePage()
+                .openHomePage()
+                .acceptCookies()
+                .clickLoginButton();
+
+        app.getLoginPage()
+                .clickLoginWithEmail()
+                .fillEmail("")
+                .fillPassword("AitWebsiteCohort82!!")
+                .clickLoginButton();
+
+        Assert.assertEquals(
+                app.getLoginPage().getEmailValidationMessage(),
+                "Email cannot be blank"
+        );
+    }
+
+    @Test
+    public void loginWithEmptyPasswordNegativeTest() {
+        app.getHomePage()
+                .openHomePage()
+                .acceptCookies()
+                .clickLoginButton();
+
+        app.getLoginPage()
+                .clickLoginWithEmail()
+                .fillEmail("aitwebsitecohort82m@gmail.com")
+                .fillPassword("")
+                .clickLoginButton();
+
+        Assert.assertEquals(
+                app.getLoginPage().getPasswordValidationMessage(),
+                "Make sure you enter a password."
+        );
+    }
+
+
 }
